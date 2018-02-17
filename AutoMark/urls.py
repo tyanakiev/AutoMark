0d4f09c3ecb4656a87815c4e50a668d64653dd10
Noonewins <trifon.yanakiev@gmail.com>
2018-02-17 16:03:40 -0600

    Adding all local files since creating this project.

---
+++ AutoMark/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    url(r'^$', auth_views.login, {'template_name': 'index.html'}),
+    url(r'^automark/twitter', views.twitter, name='twitter'),
+    url(r'^automark/facebook', views.facebook, name='facebook'),
+    url(r'^automark/statistics', views.stats, name='stats'),
+    url(r'^register/', views.register),
+]
---
+++ AutoMark/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from django import forms
+from .forms import UserRegistrationForm
+
+# Create your views here.
+
+
+
+def twitter(request):
+    return render(request, 'twitter.html')
+
+def facebook(request):
+    return render(request, 'facebook.html')
+
+def stats(request):
+    return render(request, 'stats.html')
+
+def automark(request):
+    return render(request, 'base.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            userObj = form.cleaned_data
+            username = userObj['username']
+            password =  userObj['password']
+            if not (User.objects.filter(username=username).exists()):
+                User.objects.create_user(username, password)
+                user = authenticate(username = username, password = password)
+                login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                raise forms.ValidationError('Looks like a username with that email or password already exists')
+
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'register.html', {'form' : form})
+
